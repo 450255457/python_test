f39a699031aaf1c0f7ba80297ead005e12294882
450255457 <450255457@qq.com>
2016-03-19 16:57:16 +0800

    03191657

--- python_test.py
+++ python_test.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python2.7.6
 # -*- coding: utf-8 -*-
 
-import socket
+import sys
 
-hostname = 'baidu.com'
-addr = socket.gethostbyname(hostname)
-print 'the address of', hostname, 'is', addr
+print(sys.argv[0])
