18c9f4711245a5186413b04041f11c67aab2ce18
450255457 <450255457@qq.com>
2016-04-28 16:12:18 +0800

    04281612

--- tcp_server.py
+++ tcp_server.py
@@ -1,31 +1,39 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
-import socketserver
+'''
+Created on 2016/4/28
+File Name:tcp_server.py
+author: LindenTao
+Description : a so easy tcp server
+'''
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
-	"""
-    The request handler class for our server.
+import socket
+import threading
+import time
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
+def tcplink(sock, addr):
+    print 'Accept new connection from %s:%s...' % addr
+    while True:
+        data = sock.recv(1024)
+        print('recvbuf :%s'% data)
+        time.sleep(1)
+        if data == 'exit' or not data:
+            break
+        sock.send('Hello, %s!' % data)
+    sock.close()
+    print 'Connection from %s:%s closed.' % addr
+
+# 创建socket  
+sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 监听端口:
+sockfd.bind(('0.0.0.0', 8090))
+sockfd.listen(5)
+print 'Waiting for connection...'
+while True:
+    # 接受一个新连接:
+    sock, addr = sockfd.accept()
+    # 创建新线程来处理TCP连接:
+    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t.start()
     
-	def handle(self):
-		# self.request is the TCP socket connected to the client
-		self.data = self.request.recv(1024).strip()
-		print("{}wrote:".format(self.client_address[0]))
-		print(self.data)
-		# just send back the same data, but upper-cased
-		self.request.sendall(self.data.upper())
-		
-if __name__ == '__main__':
-	HOST,PORT = "localhost", 9999
-	
-	# Create the server, binding to localhost on port 9999
-	server = socketserver.TCPServer((HOST,PORT), MyTCPHandler)
-	
-	# Activate the server; this will keep running until you
-  # interrupt the program with Ctrl-C
-	server.serve_forever()
